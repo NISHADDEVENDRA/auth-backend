5b82693bbc4b5f98668b379cadce5c659de894dc
NISHADDEVENDRA <130331341+NISHADDEVENDRA@users.noreply.github.com>
2024-09-25 22:13:41 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ import authRoutes from "./routes/auth.route.js"
 dotenv.config()
 const app=express()
 const PORT = process.env.PORT || 5000
-app.use(cors({origin :"http://localhost:5173",credentials:true}))
+app.use(cors())
 axios.defaults.withCredentials = true
 
 app.use(express.json())
